6cc9e94c795f00b23b3e8579c33eb3da3870f641
Logesh Traq <logesh.mg@traqmetrix.com>
2025-08-07 11:23:21 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express on Cloud Run without Docker!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
