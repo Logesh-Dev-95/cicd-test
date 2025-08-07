5759754d525a33d4645fd1fdba4ee9a4667540c6
Logesh Traq <logesh.mg@traqmetrix.com>
2025-08-07 11:34:06 +0530

    updated code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express on Cloud Run without Docker!');
+  res.send('Hello from Express ');
 });
 
 app.listen(PORT, () => {
